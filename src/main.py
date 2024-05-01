6a895687ab199c2c23cf180b1f8ee08c47e08659
James Bostock <77472194+james-bostock-cx@users.noreply.github.com>
2024-05-01 15:33:53 +1000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 
 # Random comment to trigger scan
+# Random comment to trigger scan
 if __name__ == '__main__':
     filename = input('Enter name of file to remove:')
     os.remove(filename)
